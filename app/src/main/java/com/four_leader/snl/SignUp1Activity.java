f3cc3bfe923e36f0595d1c91b684e134325ecc7f
garam <garam1362@naver.com>
2019-01-19 18:20:19 +0900

    second commit

--- app/src/main/java/com/four_leader/snl/SignUp1Activity.java
+++ app/src/main/java/com/four_leader/snl/SignUp1Activity.java
@@ -3,15 +3,11 @@ package com.four_leader.snl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUp1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        //다시수정
-
-
+        setContentView(R.layout.activity_sign_up1);
     }
 }
