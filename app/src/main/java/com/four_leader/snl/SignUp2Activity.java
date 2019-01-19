1ece87b18046a14f807a0f91f7ab9d8668715993
garam <garam1362@naver.com>
2019-01-19 19:03:34 +0900

    Merge branch 'master' of /Users/garam/Desktop/SNL with conflicts.

---
+++ app/src/main/java/com/four_leader/snl/SignUp2Activity.java
@@ -0,0 +1,13 @@
+package com.four_leader.snl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+    }
+}
