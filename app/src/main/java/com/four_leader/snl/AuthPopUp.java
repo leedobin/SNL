48c03c9340ea5cac84065a284489afc515daa7f1
Leedobin <dlehqls369@naver.com>
2019-01-26 20:38:06 +0900

    권한 팝업

---
+++ app/src/main/java/com/four_leader/snl/AuthPopUp.java
@@ -0,0 +1,58 @@
+package com.four_leader.snl;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+public class AuthPopUp extends AppCompatActivity {
+
+    private Button nextBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_auth);
+
+        nextBtn = findViewById(R.id.nextBtn);
+
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TedPermission.with(AuthPopUp.this)
+                        .setPermissionListener(permissionListener)
+                        .setDeniedMessage("[설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+                        .setPermissions(Manifest.permission.CAMERA)
+                        .check();
+
+
+            }
+        });
+    }
+
+    PermissionListener permissionListener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(getApplicationContext(), "권한 허가", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(AuthPopUp.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(getApplicationContext(), "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(AuthPopUp.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+    };
+}
