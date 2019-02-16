483954e0fc3bd5c4ee1ed66837fc92b44b02ebdf
Leedobin <dlehqls369@naver.com>
2019-02-16 14:28:00 +0900

    02/16 ㄷㅂ

--- app/src/main/java/com/four_leader/snl/signup/activity/SignUp1Activity.java
+++ app/src/main/java/com/four_leader/snl/signup/activity/SignUp1Activity.java
@@ -1,11 +1,12 @@
 package com.four_leader.snl.signup.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import com.four_leader.snl.R;
 
@@ -14,6 +15,7 @@ public class SignUp1Activity extends AppCompatActivity {
     private Button nextBtn;
     private ImageButton backBtn;
     private int REQUEST_CODE = 200;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,20 @@ public class SignUp1Activity extends AppCompatActivity {
 
         nextBtn = findViewById(R.id.nextBtn);
         backBtn = findViewById(R.id.backBtn);
+        relativeLayout = findViewById(R.id.checkId);
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignUp1Activity.this, SignUp2Activity.class);
                 startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
