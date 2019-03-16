9342e987748ae88da8e05cd4836e19f3f4ac4778
garam <garam1362@naver.com>
2019-03-16 16:41:21 +0900

    2019-03-16 last commit2

--- app/src/main/java/com/four_leader/snl/notice/activity/NoticeActivity.java
+++ app/src/main/java/com/four_leader/snl/notice/activity/NoticeActivity.java
@@ -21,70 +21,13 @@ import java.util.ArrayList;
 
 public class NoticeActivity extends AppCompatActivity {
 
-    ListView listView;
-    ArrayList<Notice> notices;
-    NoticeAdapter adapter;
-
-    TextView mainBtn, libraryBtn, settingBtn;
-    RelativeLayout noticeBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
 
-        mainBtn = findViewById(R.id.mainBtn);
-        libraryBtn = findViewById(R.id.libraryBtn);
-        settingBtn = findViewById(R.id.settingBtn);
-        noticeBtn = findViewById(R.id.noticeBtn);
-        listView = findViewById(R.id.listView);
 
-        getNotices();
-
-        mainBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setResult(0);
-                finish();
-            }
-        });
-
-        libraryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setResult(1);
-                finish();
-            }
-        });
-
-        settingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setResult(3);
-                finish();
-            }
-        });
     }
 
-    private void getNotices() {
-        notices = new ArrayList<>();
-        for(int i=0; i<10; i++) {
-            Notice notice = new Notice(String.valueOf(i), "user"+i+"님이\n글을 좋아요 했습니다.");
-            notices.add(notice);
-        }
 
-        adapter = new NoticeAdapter(getApplicationContext(), notices, clickItem);
-        listView.setAdapter(adapter);
-    }
-
-    Handler clickItem = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-            Intent intent = new Intent(NoticeActivity.this, ContentActivity.class);
-            intent.putExtra("code", notices.get(msg.what).getNum());
-            startActivity(intent);
-        }
-    };
 }
--- app/src/main/java/com/four_leader/snl/notice/fragment/NoticeFragment.java
+++ app/src/main/java/com/four_leader/snl/notice/fragment/NoticeFragment.java
@@ -1,15 +1,28 @@
 package com.four_leader.snl.notice.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.four_leader.snl.R;
+import com.four_leader.snl.content.activity.ContentActivity;
+import com.four_leader.snl.notice.activity.NoticeActivity;
+import com.four_leader.snl.notice.adapter.NoticeAdapter;
+import com.four_leader.snl.notice.vo.Notice;
+
+import java.util.ArrayList;
 
 public class NoticeFragment extends Fragment {
 
+    ListView listView;
+    ArrayList<Notice> notices;
+    NoticeAdapter adapter;
 
     public NoticeFragment() {
         // Required empty public constructor
@@ -31,6 +44,33 @@ public class NoticeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_notice, container, false);
+
+        listView = v.findViewById(R.id.listView);
+
+        getNotices();
         return v;
     }
+
+    private void getNotices() {
+        notices = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            Notice notice = new Notice(String.valueOf(i), "user"+i+"님이\n글을 좋아요 했습니다.");
+            notices.add(notice);
+        }
+
+        adapter = new NoticeAdapter(getActivity().getApplicationContext(), notices, clickItem);
+        listView.setAdapter(adapter);
+    }
+
+    Handler clickItem = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            Intent intent = new Intent(getActivity(), ContentActivity.class);
+            intent.putExtra("code", notices.get(msg.what).getNum());
+            startActivity(intent);
+        }
+    };
+
 }
