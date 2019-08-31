1982ce8b705a394e441680bb6cf3204a926cee9c
kangseok <kangseok06@gmail.com>
2019-08-31 14:19:07 +0900

    Merge branch 'temp' of C:\Users\kangseok\AndroidStudioProjects\SNL with conflicts.

--- app/src/main/java/com/four_leader/snl/content/activity/RecordActivity.java
+++ app/src/main/java/com/four_leader/snl/content/activity/RecordActivity.java
@@ -1,9 +1,14 @@
 package com.four_leader.snl.content.activity;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -11,9 +16,24 @@ import android.widget.Toast;
 
 import com.four_leader.snl.R;
 import com.four_leader.snl.main.vo.MainContent;
+import com.four_leader.snl.util.APIClient;
+import com.four_leader.snl.util.APIInterface;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -22,12 +42,24 @@ public class RecordActivity extends AppCompatActivity {
     ImageButton stopRecordBtn;
     TextView textView;
     MediaRecorder recorder = new MediaRecorder();
+    APIInterface apiInterface;
+
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+    String upLoadServerUri = null;
+
+    final String uploadFilePath = "storage/emulated/0/";
+    final String uploadFileName = "recorder.mp3";
+
+    String  commentSeq, scriptSeq, userSeq, commentFilename;
     MainContent content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        apiInterface = APIClient.getClient().create(APIInterface.class);
 
         closeViewBtn = findViewById(R.id.closeViewBtn);
         startRecordBtn = findViewById(R.id.startRecordBtn);
@@ -37,6 +69,8 @@ public class RecordActivity extends AppCompatActivity {
         content = (MainContent) getIntent().getSerializableExtra("content");
         textView.setText(content.getContent());
 
+
+        upLoadServerUri = "http://13.209.4.115/SNL/UPLOAD/UploadToServer.php";
 
         closeViewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,28 +90,30 @@ public class RecordActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 stopRecord();
+                saveRecord();
             }
         });
+
+
     }
 
-    public void startRecord(){
+    public void startRecord() {
         try {
 
-            File file= Environment.getExternalStorageDirectory();
-//갤럭시 S4기준으로 /storage/emulated/0/의 경로를 갖고 시작한다.
-            String path=file.getAbsolutePath()+"/"+"recoder.mp3";
+            File file = Environment.getExternalStorageDirectory();
+            String path = file.getAbsolutePath() + "/" + "recorder.mp3";
 
             recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//첫번째로 어떤 것으로 녹음할것인가를 설정한다. 마이크로 녹음을 할것이기에 MIC로 설정한다.
+            Log.d("uploadFile", "mic");
             recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//이것은 파일타입을 설정한다. 녹음파일의경우 3gp로해야 용량도 작고 효율적인 녹음기를 개발할 수있다.
+            Log.d("uploadFile", "3gp");
             recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//이것은 코덱을 설정하는 것이라고 생각하면된다.
+            Log.d("uploadFile", "amr");
             recorder.setOutputFile(path);
-//저장될 파일을 저장한뒤
+            Log.d("uploadFile", "set");
             recorder.prepare();
+            Log.d("uploadFile", "prepare");
             recorder.start();
-//시작하면된다.
             Toast.makeText(this, "start Record", Toast.LENGTH_LONG).show();
         } catch (IllegalStateException e) {
 // TODO Auto-generated catch block
@@ -88,9 +124,231 @@ public class RecordActivity extends AppCompatActivity {
         }
     }
 
-    public void stopRecord(){
+    public void stopRecord() {
         recorder.stop();
         recorder.release();
-        Toast.makeText(this,"stop Record",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "stop Record", Toast.LENGTH_LONG).show();
+    }
+
+    public void saveRecord() {
+        dialog = ProgressDialog.show(RecordActivity.this, "", "Uploading file...", true);
+
+        new Thread(new Runnable() {
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ;
+                    }
+                });
+                uploadFile(uploadFilePath + uploadFileName);
+
+
+                commentSeq="20";
+                scriptSeq="21";
+                userSeq="22";
+                commentFilename=uploadFileName;
+
+
+                recordInfo(commentSeq, scriptSeq, userSeq, commentFilename);
+            }
+        }).start();
+
+        Log.d("uploadFile", "saveRecord success!!!!!!!");
+
+        Toast.makeText(this, "save Record", Toast.LENGTH_LONG).show();
+    }
+
+
+    public int uploadFile(String sourceFileUri) {
+
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :" + uploadFilePath + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+
+            return 0;
+
+        } else {
+            try {
+                // open a URL connection to the Servlet
+
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("upload_file", fileName);
+
+                Log.d("uploadFile", "upload setting success!!!!!!!");
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+                dos.writeBytes("Content-Disposition: form-data; name=\"upload_file\";filename=\"" + fileName + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
+
+                if (serverResponseCode == 200) {
+
+                    runOnUiThread(new Runnable() {
+
+                        public void run() {
+
+                            //String msg = "File Upload Completed.\n\n See uploaded file here : \n\n" + uploadFileName;
+
+                            Toast.makeText(RecordActivity.this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+
+                //close the streams //
+
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+
+                    public void run() {
+
+                        Toast.makeText(RecordActivity.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+
+            } catch (Exception e) {
+
+                dialog.dismiss();
+
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+
+                    public void run() {
+                        Toast.makeText(RecordActivity.this, "Got Exception : see logcat ", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+                Log.e("Upload file Exception", "Exception : " + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+        } // End else block
+    }
+
+    private void recordInfo(final String commentSeq, final String scriptSeq, final String userSeq, final String commentFilename) {
+        Call<String> call = apiInterface.recordInfo(commentSeq, scriptSeq, userSeq, commentFilename);
+        call.enqueue(new Callback<String>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                String result = response.body().toString();
+
+                try {
+
+                    JSONObject resultObj = new JSONObject(result);
+                    JSONArray report = resultObj.getJSONArray("REPORT");
+
+                    JSONObject obj = new JSONObject(report.get(0).toString());
+                    String user_seq = obj.getString("user_seq");
+
+                    SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+
+                    editor.putString("comment_seq", commentSeq);
+                    editor.putString("script_seq", scriptSeq);
+                    editor.putString("user_seq", userSeq);
+                    editor.putString("comment_file_name", commentFilename);
+
+                    Log.i("stag", "user_seq : " + user_seq);
+
+                    editor.commit();
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                call.cancel();
+                Toast.makeText(getApplicationContext(), "서버 연결 실패", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/four_leader/snl/util/APIInterface.java
+++ app/src/main/java/com/four_leader/snl/util/APIInterface.java
@@ -1,9 +1,5 @@
 package com.four_leader.snl.util;
 
-import com.google.gson.JsonObject;
-
-import org.json.JSONObject;
-
 import java.util.List;
 
 import retrofit2.Call;
@@ -38,80 +34,10 @@ public interface APIInterface {
     Call<String> setUserCategory(@Query("user_seq") String userSeq,
                                  @Query("category_seq") String categorySeq);
 
-    // 유저 권한 허가 상태 조회
-    @GET("AGREEMENT/getAgreement.php")
-    Call<String> checkUserPermission(@Query("user_seq") String userSeq);
-
-    // 카테고리 시퀀스에 따라 글 목록 조회(메인)
-    @GET("SCRIPT/scriptList.php")
-    Call<String> getScriptByCateSeq(@Query("category_seq") String categorySeq,
-                                    @Query("user_seq") String userSeq);
-
-    // 푸쉬권한 동의/거절
-    @GET("USER/push_permission.php")
-    Call<String> setPushPermission(@Query("userId") String userId,
-                                   @Query("date") String date);
-
-    // 해당 게시글의 댓글 목록 조회
-    @GET("SCRIPT/DetailsOfScript.php")
-    Call<String> getScriptComment(@Query("scriptSeq") String scriptSeq);
-
-    // 글쓰기
-    @GET("SCRIPT/writeScript.php")
-    Call<String> writeScript(@Query("user_seq") String userSeq,
-                             @Query("script_title") String title,
-                             @Query("script_content") String content,
-                             @Query("script_category_code") int categorySeq);
-
-    // 글 좋아요
-    @GET("SCRIPT/like.php")
-    Call<String> addLike(@Query("user_seq") String userSeq,
-                         @Query("script_seq") String scriptSeq);
-
-    // 글 좋아요 취소
-    @GET("SCRIPT/unlike.php")
-    Call<String> deleteLike(@Query("user_seq") String userSeq,
-                            @Query("script_seq") String scriptSeq);
-
-    // 글 공유
-    @GET("SCRIPT/share.php")
-    Call<String> share(@Query("user_seq") String userSeq,
-                       @Query("script_seq") String scriptSeq,
-                       @Query("user_id") String userId);
-
-    // 글 공유 취소
-    @GET("SCRIPT/unshare.php")
-    Call<String> unshare(@Query("user_seq") String userSeq,
-                            @Query("script_seq") String scriptSeq);
-
-    // 글 신고
-    @GET("SCRIPT/reportScript.php")
-    Call<String> reportScript(@Query("script_seq") String scriptSeq,
-                              @Query("reporting_seq") String reportingSeq,
-                              @Query("reported_seq") String reportedSeq,
-                              @Query("report_memo") String reportMemo);
-
-    // 댓글 신고
-    @GET("COMMENT/reportComment.php")
-    Call<String> reportComment(@Query("comment_seq") String commentSeq,
-                               @Query("reporting_seq") String reportingSeq,
-                               @Query("reported_seq") String reportedSeq,
-                               @Query("report_memo") String reportMemo);
-
-
-    // 서재 - 내글
-    @GET("SCRIPT/myLib.php")
-    Call<String> getMyLib(@Query("user_seq") String userSeq);
-
-    // 서재 - 공유
-    @GET("SCRIPT/shareLib.php")
-    Call<String> getShareLib(@Query("user_seq") String userSeq);
-
-    // 알람 목록 조회
-    @GET("ALARM/alarmList.php")
-    Call<String> getAlarmList(@Query("user_seq") String userSeq);
-
-    // 알람 읽음 처리
-    @GET("ALARM/alarmRead.php")
-    Call<String> setAlarmRead(@Query("alarm_seq") String alarmSeq);
+    //댓글 음성 파일 정보 전송
+    @GET("COMMENT/writeComment.php")
+    Call<String> recordInfo(@Query("comment_seq") String commentSeq,
+                            @Query("script_seq") String scriptSeq,
+                            @Query("user_seq") String userSeq,
+                            @Query("comment_file_name") String commentFilename);
 }
