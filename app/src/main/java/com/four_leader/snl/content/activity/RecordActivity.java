12b6e50ae1dc4455a33dac3315704410c93daf4e
kangseok <kangseok06@gmail.com>
2019-08-31 16:25:56 +0900

    test

--- app/src/main/java/com/four_leader/snl/content/activity/RecordActivity.java
+++ app/src/main/java/com/four_leader/snl/content/activity/RecordActivity.java
@@ -150,7 +150,6 @@ public class RecordActivity extends AppCompatActivity {
                 commentFilename=uploadFileName;
 
 
-
                 recordInfo(commentSeq, scriptSeq, userSeq, commentFilename);
             }
         }).start();
